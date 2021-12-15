df747a52701cc16872f72f2d2a579f7dc3deabbc
adrian <adrian.rebollar.06@gmail.com>
2021-12-15 11:56:10 +0100

    metodo nuevo metodo imprimirListaNumeradaDeAlumnos con clase test

--- test.java
+++ test.java
@@ -6,24 +6,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * The test class ClaseDamTest.
+ * The test class test.
  *
  * @author  (your name)
  * @version (a version number or a date)
  */
-public class ClaseDamTest
+public class test
 {
     private Alumno alumno1;
     private Alumno alumno2;
     private Alumno alumno3;
     private Alumno alumno4;
     private Alumno alumno5;
-    private ClaseDam Dam;
+    private ClaseDam claseDam1;
 
     /**
-     * Default constructor for test class ClaseDamTest
+     * Default constructor for test class test
      */
-    public ClaseDamTest()
+    public test()
     {
     }
 
@@ -36,12 +36,18 @@ public class ClaseDamTest
     public void setUp()
     {
         alumno1 = new Alumno("Adrian", "0001", 21);
-        alumno2 = new Alumno("treceño", "0002", 20);
-        alumno3 = new Alumno("aaron perez", "0003", 19);
-        alumno4 = new Alumno("Sabina", "0004", 26);
+        alumno2 = new Alumno("Sabina", "0002", 26);
+        alumno3 = new Alumno("treceño", "0003", 20);
+        alumno4 = new Alumno("Aaron perez", "0004", 19);
         alumno5 = new Alumno("Andrea", "0005", 20);
-        Dam = new ClaseDam(30);
-        
+        claseDam1 = new ClaseDam(30);
+        claseDam1.matricularAlumno(alumno1);
+        claseDam1.matricularAlumno(alumno2);
+        claseDam1.matricularAlumno(alumno3);
+        claseDam1.matricularAlumno(alumno4);
+        claseDam1.matricularAlumno(alumno5);
+        claseDam1.imprimeDetalles();
+        claseDam1.imprimirListaNumeradaDeAlumnos();
     }
 
     /**
